e40ff48ba92135341b118a8e0f209e4836f88495
shivamkrishna <74373566+shivamkrishna@users.noreply.github.com>
2021-03-05 22:37:25 +0530

    ci cd

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ import appManager from './app';
 import './errors';
 import path from 'path';
 import kue from './kue';
+const cmd = require("node-cmd");
 
 global.appRoot = path.resolve(__dirname);
 
@@ -21,7 +22,21 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use('/api', restRouter);
-
+app.post('/git', (req, res) => {
+	// If event is "push"
+	if (req.headers['x-github-event'] == "push") {
+		cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+		cmd.get('./git.sh', (err, data) => {  // Run our script
+		  if (data) console.log(data);
+		  if (err) console.log(err);
+		});
+		cmd.run('refresh');  // Refresh project
+	  
+		console.log("> [GIT] Updated with origin/master");
+	  }
+  
+	return res.sendStatus(200); // Send back OK status
+});
 app.use((req, res, next) => {
   console.log(req.url)
 	next(new RequestError('Invalid route', 404));
